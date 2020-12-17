dded9a399ba39644e062626df7932e76861b1c85
Gwe2B <gwenael2b@live.fr>
2020-12-17 11:01:07 +0100

    Add config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define("DB_CFG", array(
+    'HOST'     => "localhost",
+    'DB_NAME'  => "SITEC",
+    'USER'     => "root",
+    'PASSWORD' => ""
+));
+
+define("PHPMAILER_CFG", array(
+    'HOST' => "smtp.gmail.com",
+    'PORT' => 587,
+    'USER' => "equipe8.hackaton2020@gmail.com",
+    'PASS' => "hackaton2020"
+));
