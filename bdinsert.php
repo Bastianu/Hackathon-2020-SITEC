96ef1caf98e0709a0e6630eb19447272861925a7
Bastianu <sb_bastianu@orange.fr>
2020-12-18 14:17:34 +0100

    fix

--- bdinsert.php
+++ bdinsert.php
@@ -10,13 +10,13 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $sql = "INSERT INTO USER (firstname, lastname, rol, email, passwd)
-  VALUES ('Mickael', 'Rigonnaux', 'administrateur','mickael@example.com', '".password_hash('administrateur',PASSWORD_BCRYPT)."')";
+  VALUES ('Mickael', 'Rigonnaux', 'administrateur','admin@admin.com', '".password_hash('administrateur',PASSWORD_BCRYPT)."')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully<br>";
 
   $sql = "INSERT INTO USER (firstname, lastname, rol, email, passwd)
-  VALUES ('John', 'Washington', 'client','washington@example.com', '".password_hash('client',PASSWORD_BCRYPT)."')";
+  VALUES ('John', 'Washington', 'client','client@client.com', '".password_hash('client',PASSWORD_BCRYPT)."')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully<br>";
@@ -134,6 +134,19 @@ try {
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully<br>";
+
+  $sql = "INSERT INTO CVE (cve_id, description, severity, solution, name)
+  VALUES ('CVE-2010-3333','This type of attack leverages the use of tags or variables from a formatted configuration data to cause buffer overflow. The attacker crafts a malicious HTML page or configuration file that includes oversized strings, thus causing an overflow.' ,'9.3','Use a language or compiler that performs automatic bounds checking. Use an abstraction library to abstract away risky APIs. Not a complete solution. Compiler-based canary mechanisms such as StackGuard, ProPolice and the Microsoft Visual Studio /GS flag. Unless this provides automatic bounds checking, it is not a complete solution. Use OS-level preventative functionality. Not a complete solution. Do not trust input data from user. Validate all user input.','Buffer Overflow in an API Call')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully<br>";
+
+  $sql = "INSERT INTO CVETECHNO (id_cve, id_techno)
+  VALUES ('CVE-2010-3333',1)";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully<br>";
+
   
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
